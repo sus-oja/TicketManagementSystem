8e87ca0e24435e892fcab7994c19766b6f0ab79d
AirikaT <airika.torn@gmail.com>
2020-11-01 13:01:52 +0200

    AT clas Client and Hibernate

---
+++ src/main/java/model/Client.java
@@ -0,0 +1,93 @@
+package model;
+
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(schema = "theatre", name = "clients")
+
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Column(name = "client_id")
+    private int clientId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "last_updated")
+    private Date lastUpdated;
+
+    public Client(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+}
--- src/main/java/util/DBUtil.java
+++ src/main/java/util/DBUtil.java
@@ -1,4 +1,43 @@
 package util;
 
+import model.Client;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+import java.util.Properties;
+
 public class DBUtil {
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        try{
+            Configuration configuration = new Configuration();
+            Properties properties = new Properties();
+
+            properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+            properties.put(Environment.URL, "jdbc:mysql://localhost:3306/theatre?createDatabaseIfNotExist=true&serverTimezone=UTC");
+            properties.put(Environment.USER, "root");
+            properties.put(Environment.PASS, "ThureC6E!");
+            properties.put(Environment.SHOW_SQL, "true");
+            properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
+            properties.put(Environment.HBM2DDL_AUTO, "update");
+            properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+            configuration.setProperties(properties);
+
+            configuration.addAnnotatedClass(Client.class);
+
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().
+                    applySettings(configuration.getProperties()).build();
+
+            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return sessionFactory;
+    }
+
 }
