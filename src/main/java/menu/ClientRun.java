b9bf002c5938cedf7b899d421bf637d77d5f2f08
susoja <susanna.oja@hotmail.com>
2020-11-02 21:14:51 +0200

    Merging devALL and devSO

---
+++ src/main/java/menu/ClientRun.java
@@ -0,0 +1,28 @@
+package menu;
+
+import model.Client;
+import repository.ClientDao;
+import java.util.Scanner;
+
+public class ClientRun {
+
+    public static void main(String[] args) {
+        Client newClient = new Client();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give first name: ");
+        newClient.setFirstName(scanner.nextLine());
+
+        System.out.println("Give last name: ");
+        newClient.setLastName(scanner.nextLine());
+
+        System.out.println("Give email: ");
+        newClient.setEmail(scanner.nextLine());
+
+        //System.out.println(newClient.getFirstName());
+
+        ClientDao clientDao = new ClientDao();
+        clientDao.createClient(newClient);
+
+    }
+}
