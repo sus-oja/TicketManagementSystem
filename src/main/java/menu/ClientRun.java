f7e806bcb4cdfbe30e2fc2b200e4a656beff4546
AirikaT <airika.torn@gmail.com>
2020-11-02 13:01:09 +0200

    Update ClientRun.java

--- src/main/java/menu/ClientRun.java
+++ src/main/java/menu/ClientRun.java
@@ -7,23 +7,24 @@ import java.util.Scanner;
 
 public class ClientRun {
 
-        public static void main(String[] args) {
-            Client newClient = new Client();
+    public static void main(String[] args) {
+        Client newClient = new Client();
 
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Give first name: ");
-            newClient.setFirstName(scanner.nextLine());
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give first name: ");
+        newClient.setFirstName(scanner.nextLine());
 
-            System.out.println("Give last name: ");
-            newClient.setLastName(scanner.nextLine());
+        System.out.println("Give last name: ");
+        newClient.setLastName(scanner.nextLine());
 
-            System.out.println("Give email: ");
-            newClient.setEmail(scanner.nextLine());
+        System.out.println("Give email: ");
+        newClient.setEmail(scanner.nextLine());
 
-            //System.out.println(newClient.getFirstName());
+        //System.out.println(newClient.getFirstName());
 
-            ClientDao clientDao = new ClientDao();
-            clientDao.createClient(newClient);
+        ClientDao clientDao = new ClientDao();
+        clientDao.createClient(newClient);
 
 
-        }
+    }
+}
